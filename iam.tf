46161c5bb7470464102b97fe421adba6ea72c939
hoalongnatsu <hoalongnatsu@gmail.com>
2021-07-23 22:28:44 +0700

    init

--- iam.tf
+++ iam.tf
@@ -21,7 +21,7 @@ data "aws_iam_policy_document" "policy_doc" {
   }
 }
 
-data "aws_iam_policy" "policy" {
+resource "aws_iam_policy" "policy" {
   name   = "${local.namespace}-tf-policy"
   path   = "/"
   policy = data.aws_iam_policy_document.policy_doc.json
@@ -50,5 +50,5 @@ resource "aws_iam_role" "iam_role" {
 
 resource "aws_iam_role_policy_attachment" "policy_attach" {
   role       = aws_iam_role.iam_role.name
-  policy_arn = aws_iam_policy.iam_policy.arn
+  policy_arn = aws_iam_policy.policy.arn
 }
